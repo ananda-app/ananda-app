d7c1085a1451a8cac352884847ba41995ccb30fe
Rajiv Poddar <rajiv@gimlet.ai>
2024-06-21 20:33:24 +0530

    meta descriptoin

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const WebsiteName: string = "Ananda"
 export const WebsiteBaseUrl: string = "https://ananda.app"
-export const WebsiteDescription: string = "Ananda is an interactive guided meditation app that leverages computer vision to evaluate mental state and posture, offering tailored instructions accordingly"
+export const WebsiteDescription: string = "Interactive guided meditation app, using computer vision and AI assess mental state and posture, offering tailored instructions accordingly"
