d5766e295f0c85721995cd5e2f1b6a484a6aef60
Rajiv Poddar <rajiv@gimlet.ai>
2024-06-21 20:02:11 +0530

    responsive

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -33,7 +33,7 @@
         tabindex="0"
         class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-bold"
       >
-        <li><a href="/account">Account</a></li>
+        <li><a href="https://tally.so/r/w7DOZP">Waitlist</a></li>
       </ul>
     </div>
   </div>
